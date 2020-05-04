8697e28b1dfa24d40cb4de77d43432b20d97733f
Alisa <alisa.grbic@gmail.com>
2020-05-04 23:15:42 +0200

    Merge branch 'master' of https://github.com/alisagrbic/Android
     Conflicts:
    	app/build.gradle
    	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/dance_world/MapsActivity.java
+++ app/src/main/java/com/example/dance_world/MapsActivity.java
@@ -1,17 +1,16 @@
 package com.example.dance_world;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
