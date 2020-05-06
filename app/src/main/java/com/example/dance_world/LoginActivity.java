5d3339e3592d0c3951c4fa617440b4c0909186ca
Alisa <alisa.grbic@gmail.com>
2020-05-06 14:56:37 +0200

    Welcome

--- app/src/main/java/com/example/dance_world/LoginActivity.java
+++ app/src/main/java/com/example/dance_world/LoginActivity.java
@@ -1,34 +1,35 @@
 package com.example.dance_world;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ListView;
 
 import com.example.dance_world.database.DatabaseHelper;
 import com.example.dance_world.database.entities.User;
 
+public class LoginActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-    ImageButton imageButtonWelcome;
+    private DatabaseHelper helper;
+    Button SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        imageButtonWelcome = findViewById(R.id.imageButtonWelcome);
+        helper = DatabaseHelper.getInstance(this);
+        User user = new User("Alisa", "Grbic", "Alisic", "alisa.grbic@gmail.com", "alisa123", 0);
+        helper.UserDao().insertUser(user);
 
-        imageButtonWelcome.setOnClickListener(new View.OnClickListener() {
+        SignUp = findViewById(R.id.SignUp);
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
