d9e55fbb6fac262283d4d7a8d302e1812ef9ac11
Alisa <alisa.grbic@gmail.com>
2020-05-08 21:28:08 +0200

    Click listener

--- app/src/main/java/com/example/dance_world/DetailActivity.java
+++ app/src/main/java/com/example/dance_world/DetailActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+
+import com.example.dance_world.database.entities.Artist;
 
 public class DetailActivity extends AppCompatActivity {
 
     ImageButton settings, liness;
+    Button artist, dj, workshop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,9 @@ public class DetailActivity extends AppCompatActivity {
 
         settings = findViewById(R.id.settings);
         liness = findViewById(R.id.liness);
+        artist = findViewById(R.id.artist);
+        dj = findViewById(R.id.dj);
+        workshop = findViewById(R.id.workshop);
 
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +38,27 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DetailActivity.this, NavigationActivity.class);
+                startActivity(intent);
+            }
+        });
+        artist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailActivity.this, ArtistsActivity.class);
+                startActivity(intent);
+            }
+        });
+        dj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailActivity.this, DjActivity.class);
+                startActivity(intent);
+            }
+        });
+        workshop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailActivity.this, WorkshopActivity.class);
                 startActivity(intent);
             }
         });
